bf8eff0d402b015a8857b50ed2f6ed88e7c673b6
pampersnow <17611309300@163.com>
2018-12-25 15:25:04 +0800

    code update

---
+++ src/main/java/com/sys/pro/dto/ResponseInfo.java
@@ -0,0 +1,40 @@
+package com.sys.pro.dto;
+
+import java.io.Serializable;
+
+/**
+ * @author JYB
+ * @date 2018.10
+ * @version 1.0
+ * @parameter ResponseInfo 
+ */
+public class ResponseInfo implements Serializable {
+	
+	private static final long serialVersionUID = -4417715614021482064L;
+
+	private String code;
+	private String message;
+
+	public ResponseInfo(String code, String message) {
+		super();
+		this.code = code;
+		this.message = message;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
